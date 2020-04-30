fad7e0b6a15049d40f3e8b0b1bdd3c357f40a79b
Petr Manek <petrmanek@users.noreply.github.com>
2020-04-30 22:34:21 +0100

    regenerate figure 13

--- final_report/sinmodel_TBR.m
+++ final_report/sinmodel_TBR.m
@@ -1,14 +1,11 @@
-pos = get(gca, 'Position');
-set(gca, 'Position', [pos(1) pos(2)+0.01 pos(3) pos(4)-0.01]);
-
 wn=1
 
 f=@(x,y) sin(wn*2*pi*(x-0.5))/2 + sin(wn*2*pi*(y-0.5))/2 + 1;
 [X,Y]=meshgrid(0:0.001:1);
 z=f(X,Y);
 [C,h] = contourf(X,Y,z,9);   
-clabel(C,h,'FontSize',15)
-set(gca,'fontsize', 24)
+clabel(C,h,'FontSize',11)
+%set(gca,'fontsize', 24)
 
 set(gca,'xtick',[0,1])
 set(gca,'xticklabel',[0,1])
@@ -18,5 +15,3 @@ set(gca,'yticklabel',[0,1])
 
 xlabel('Continuous parameter x_1')
 ylabel('Continuous parameter x_2')
-
-savefig('asd.pdf')
