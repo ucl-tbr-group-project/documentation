6466aecfa2a57c94ccb67778bbca557982b8cc4b
Petr Manek <petrmanek@users.noreply.github.com>
2020-06-11 11:50:59 +0100

    rasterize figure 6 plots (reducing the paper PDF size 10x), add rasterization script

---
+++ paper/rasterize_figure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+DPI=300
+INPUT_FILE=$1 ; shift
+OUTPUT_FILE=$1 ; shift
+
+gs -o /tmp/onlytxt.pdf -sDEVICE=pdfwrite -dFILTERVECTOR -dFILTERIMAGE $INPUT_FILE
+gs -o /tmp/graphics.pdf -sDEVICE=pdfwrite -dFILTERTEXT $INPUT_FILE
+convert -density $DPI -quality 100 /tmp/graphics.pdf /tmp/graphics.png
+convert -density $DPI -quality 100 /tmp/graphics.png /tmp/graphics.pdf
+pdftk /tmp/graphics.pdf stamp /tmp/onlytxt.pdf output $OUTPUT_FILE
+rm /tmp/onlytxt.pdf /tmp/graphics.pdf /tmp/graphics.png
+
